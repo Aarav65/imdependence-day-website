8b416898c994edd286407d938dc5ab7284d6b047
Aarav65 <76677576+Aarav65@users.noreply.github.com>
2023-03-05 10:17:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function cal()
+{
+    curyear = document.getElementById("year").value;
+    time =  curyear - 1947;
+    document.getElementById("ans").innerHTML = time + " Year to freedom";
+    
+
+    
+}
